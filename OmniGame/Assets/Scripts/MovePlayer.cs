63e383a03c7b05aa9c99ad886eb8ed59efbe8caf
Charles McGregor <charlesmcgr@comcast.net>
2017-11-04 18:51:26 -0500

    Intial commit.

---
+++ OmniGame/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    public float moveSpeed = 1;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveVector = new Vector3(horizontal, 0, vertical);
+
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+    }
+}
